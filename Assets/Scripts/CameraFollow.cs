b5d957965862c7c4ee4eae6adb424a976a520ae0
Aaron <aaronmirotta@gmail.com>
2020-01-18 13:18:54 -0500

    Finished player movement animation, camera follow
    added camera follow script, fixed/ finished player movement animations

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.position;
+
+    }
+
+    private void Update()
+    {
+        Vector3 velocity = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, player.position + offset, ref velocity, 0.1f);
+    }
+}
